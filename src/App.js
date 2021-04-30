dd21fa72298ebf33dd3717b954efb9061847c895
mohammed ahmedin <mohammed.ahmedin@outlook.com>
2021-04-30 16:21:25 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import AppRoute from "./components/AppRoute";
+import API from "./components/API";
+
+
+
+
+// 13.00  // den här error som vi fick i useEffect 
+// assets hur kan vi rendera background image i react 
+// en valig image i react 
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"   >
+       
+    
+
+  <AppRoute/>
+      
     </div>
   );
 }
---
+++ src/components/AppRoute.js
@@ -0,0 +1,34 @@
+import React from "react";
+import CardList from "./CardList";
+import Form from "./Form";
+import Menu from "./Menu";
+import Registration from "./Registration"
+
+import {Route, BrowserRouter as Router} from "react-router-dom"
+// react-router-dom ?? 
+
+// yarn add react-router-dom
+
+export default function AppRoute(){
+
+    return(
+
+        <div>
+    
+        
+
+        <Router>  
+              
+           <Menu />
+        
+           <Route path="/" exact component={CardList} />
+           <Route path="/form" component={Form} />
+           <Route path="/register" component={Registration} />
+
+        </Router>
+           
+           
+           
+        </div>
+    )
+}
